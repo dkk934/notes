7b88042c2342e78c684f6ca164c17bf905b7076c
DK <128988989+dkk934@users.noreply.github.com>
2024-08-21 22:13:40 +0530

    Add files via upload

---
+++ src/component/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Header";
+import Footer from "./footer";
+import Note from "./note";
+
+function App() {
+    return <div>
+    <Heading />
+    <Note />
+    <Footer />
+    </div>
+};
+
+export default App;
---
+++ src/component/note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Note() {
+    return <div className="note">
+        <h1> for a title and a</h1>
+        <p>for the content.</p>
+    </div>
+};
+
+export default Note;
